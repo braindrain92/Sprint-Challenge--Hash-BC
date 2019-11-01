75880481c3b17465c8fd2f5bc4d6a0024c4f6c45
Alex <alexcc17@gmail.com>
2019-11-01 11:37:02 -0400

    Ex1 complete

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -12,7 +12,19 @@ def get_indices_of_item_weights(weights, length, limit):
     """
     YOUR CODE HERE
     """
+    # We add all weights to our hashtable
+    for i, weight in enumerate(weights):
+        hash_table_insert(ht, weight, i)
 
+    # We search for the two keys that sum to our limit
+    for i, weight in enumerate(weights):
+        result = hash_table_retrieve(ht, limit - weight)
+        if result is not None:
+            if result > i:
+                return (result, i)
+            else:
+                return (i, result)
+    
     return None
 
 
